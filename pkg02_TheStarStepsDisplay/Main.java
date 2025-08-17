a33048aa1f1e0689a3e09385a369cb8fe6ad4ecd
MuhammadSifat67 <mdsifatkhan1999@gmail.com>
2025-08-18 00:15:22 +0600

    Add files via upload

---
+++ pkg02_TheStarStepsDisplay/Main.java
@@ -0,0 +1,28 @@
+package pkg02_TheStarStepsDisplay;
+
+
+public class Main 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        int n = 5;
+        int i = 1;
+        
+        while (i<=n)
+        {
+            int j = 1;
+            while (j<=i)
+            {
+                System.out.print("*");
+                j++;
+            }
+            
+            System.out.println ();
+            i++;
+            
+        }
+    }
+    
+}
---
+++ pkg04_ThePermutationVault/Main.java
@@ -0,0 +1,22 @@
+package pkg04_ThePermutationVault;
+
+
+public class Main 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        int n = 5;
+        
+        long factorial = 1;
+        
+        for (int i=1; i<=n; i++)
+        {
+            factorial = factorial*i;
+        }
+        
+        System.out.println("The Museum Vault Key " +n+ " Unlocks in: " +factorial+ " Ways.");
+    }
+    
+}
