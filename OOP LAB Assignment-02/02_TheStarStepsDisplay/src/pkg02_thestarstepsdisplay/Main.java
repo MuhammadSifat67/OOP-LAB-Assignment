a050fe3eaef6cd1b901b831ab40e7152b59fd994
MuhammadSifat67 <mdsifatkhan1999@gmail.com>
2025-08-17 23:20:08 +0600

    Add files via upload

---
+++ OOP LAB Assignment-02/02_TheStarStepsDisplay/src/pkg02_thestarstepsdisplay/Main.java
@@ -0,0 +1,27 @@
+package pkg02_thestarstepsdisplay;
+
+public class Main 
+{
+
+   
+    public static void main(String[] args) 
+    {
+       int n = 5;
+       int i = 1;
+       
+       while (i<= n)
+       {
+           int j = 1;
+           
+           while (j<=i)
+           {
+               System.out.print("*");
+               j++;
+           }
+           
+           System.out.println ();
+           i++;
+       }
+    }
+    
+}
---
+++ OOP LAB Assignment-02/03_TheOddLanternParade/src/pkg03_theoddlanternparade/Main.java
@@ -0,0 +1,25 @@
+package pkg03_theoddlanternparade;
+
+public class Main 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        int n = 10;
+        int sum = 0;
+        int odd;
+        System.out.print("The Odd Numbers Are: ");
+        
+        for (int i= 1; i<=n; i++)
+        {
+            odd = 2*i-1;
+            System.out.print(odd + " ");
+            sum += odd;
+        }
+        
+        System.out.println();
+        System.out.println("The Sum of Odd Natural Number Upto " +n+ " Terms: " + sum);
+    }
+    
+}
